92a2f8b1338237f3c820e04f2e65eaf9dce1ac34
adenylyl <lynna165owl@gmail.com>
2022-03-17 19:06:29 -0400

    commented out env_settings ['ENGINE']

--- puzzlehunt_server/settings/env_settings.py
+++ puzzlehunt_server/settings/env_settings.py
@@ -6,8 +6,8 @@ DEBUG = os.getenv("DJANGO_ENABLE_DEBUG", default="False").lower() == "true"
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
-if(DATABASES['default']['ENGINE'] == 'django.db.backends.mysql'):
-    DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
+#if(DATABASES['default']['ENGINE'] == 'django.db.backends.mysql'):
+#    DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
 
 INTERNAL_IPS = ['127.0.0.1', 'localhost']
 EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_USER")
